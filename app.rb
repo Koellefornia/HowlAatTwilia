5f5b4752cd7d8206b40091044210bbed315dca0c
Johannes Tomin <johannes.tomin@web.de>
2015-05-09 16:35:39 +0100

    restclient

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'uri'
 require 'rest-client'
 
 #setup RestClient caching backed by Memcachier
- # RestClient.enable Rack::Cache,
- # :verbose      => true
-# :metastore   => Dalli::Client.new,
-# :entitystore => Dalli::Client.new
+RestClient.enable Rack::Cache,
+ :verbose      => true
+ :metastore   => Dalli::Client.new,
+ :entitystore => Dalli::Client.new
 
 def get_or_post(path, opts={}, &block)
 	get(path, opts, &block)
